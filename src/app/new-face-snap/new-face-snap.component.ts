2a3629e7087fdaa3c1676d8485b0e2be80a1ad63
Aymane <danielayman@gmail.com>
2022-03-19 09:28:10 +0100

    form reactive

---
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-face-snap',
+  templateUrl: './new-face-snap.component.html',
+  styleUrls: ['./new-face-snap.component.scss']
+})
+export class NewFaceSnapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
