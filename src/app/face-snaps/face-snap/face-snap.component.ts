31395dcaa1bca0627baeae6e35948bd30102ce2c
Aymane <danielayman@gmail.com>
2022-03-20 18:44:14 +0100

    Module ng

---
+++ src/app/face-snaps/face-snap/face-snap.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../../core/models/face-snap.model'
+import { FaceSnapsService } from '../../core/services/face-snaps.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent {
+  @Input() faceSnap!: FaceSnap;
+  buttonText!: string;
+
+  constructor(
+              private router: Router) {}
+
+
+
+  onViewFaceSnap() {
+    this.router.navigateByUrl(`facesnaps/${this.faceSnap.id}`);
+  }
+}
---
+++ src/app/landing-page/landing-page.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { FormsModule } from '@angular/forms';
+import { CoreModule } from '../core/core.module';
+
+
+
+@NgModule({
+  declarations: [
+    LandingPageComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
+  exports: [
+    LandingPageComponent
+  ]
+})
+export class LandingPageModule { }
