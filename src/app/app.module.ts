35bbc3065846f347cfe5a399297315c04890571c
Aymane <danielayman@gmail.com>
2022-03-21 00:33:02 +0100

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { FaceSnapsModule } from './face-snaps/face-snaps.module';
 import { LandingPageModule } from './landing-page/landing-page.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LandingPageModule } from './landing-page/landing-page.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    LandingPageModule
+    LandingPageModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
--- src/app/face-snaps/face-snaps-routing.module.ts
+++ src/app/face-snaps/face-snaps-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../core/guards/auth.guard';
 import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list.component';
 import { NewFaceSnapComponent } from './components/new-face-snap/new-face-snap.component';
 import { SingleFaceSnapComponent } from './components/single-face-snap/single-face-snap.component';
@@ -7,9 +8,9 @@ import { SingleFaceSnapComponent } from './components/single-face-snap/single-fa
 
 
 const routes: Routes = [
-  { path: 'create', component: NewFaceSnapComponent },
-  { path: ':id', component: SingleFaceSnapComponent },
-  { path: '', component: FaceSnapListComponent },
+  { path: 'create', component: NewFaceSnapComponent, canActivate: [AuthGuard] },
+  { path: ':id', component: SingleFaceSnapComponent, canActivate: [AuthGuard]  },
+  { path: '', component: FaceSnapListComponent, canActivate: [AuthGuard]  },
 ];
 
 @NgModule({
